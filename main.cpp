dc498f5889f3f09b9d0e50ee676d4261d1dad34d
Johannes Wilde <git@jowil.de>
2025-10-23 23:14:55 +0200

    Simplify to basic test case.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 // https://en.cppreference.com/w/cpp/numeric/math/div
 
-#include <cassert>
-#include <cmath>
 #include <cstdlib>
 #include <format>
 #include <iostream>
 
+#include <intrin.h>
+#include <immintrin.h>
+
 int main()
 {
-    int const dividend = 1739;
-    int const divisor = 29;
-    auto dv = std::div(dividend, divisor);
-
-    std::cout << std::format("{0} / {1} = {1} * {2} + {3}", dividend, divisor, dv.quot, dv.rem) << std::endl;
+    _div_t const dv = std::div((int)5, (int)3);
 
     return 0;
 }
