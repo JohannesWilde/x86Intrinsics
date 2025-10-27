bf817f25b97d40a69031920fd02addbe434626b9
Johannes Wilde <git@jowil.de>
2025-10-27 06:55:44 +0100

    Use fixed-sized data types - that's what the Intel documentation for the CPU instructions uses as well.

--- x86instructions.cpp
+++ x86instructions.cpp
@@ -1,11 +1,13 @@
 #include "x86instructions.hpp"
 
-std::div_t X86Instructions::idiv(int const dividend, int const divisor)
+std::div_t X86Instructions::idiv(std::int32_t dividend, std::int32_t const divisor)
 {
+    static_assert(sizeof(int) == sizeof(std::int32_t));
     return std::div(dividend, divisor);
 }
 
-std::lldiv_t X86Instructions::idiv(long long const dividend, long long const divisor)
+std::lldiv_t X86Instructions::idiv(std::int64_t const dividend, std::int64_t const divisor)
 {
+    static_assert(sizeof(long long int) == sizeof(std::int64_t));
     return std::div(dividend, divisor);
 }
--- x86instructions.hpp
+++ x86instructions.hpp
@@ -1,6 +1,7 @@
 #ifndef X86INSTRUCTIONS_HPP
 #define X86INSTRUCTIONS_HPP
 
+#include <cstdint>
 #include <cstdlib>
 
 class X86Instructions
@@ -8,9 +9,9 @@ class X86Instructions
 public:
     X86Instructions() = delete;
 
-    static std::div_t idiv(int const dividend, int const divisor);
+    static std::div_t idiv(std::int32_t const dividend, std::int32_t const divisor);
 
-    static std::lldiv_t idiv(long long int const dividend, long long int const divisor);
+    static std::lldiv_t idiv(std::int64_t const dividend, std::int64_t const divisor);
 };
 
 #endif // X86INSTRUCTIONS_HPP
